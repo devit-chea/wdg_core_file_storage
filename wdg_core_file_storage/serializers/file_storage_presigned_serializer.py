272d0b4689f0ec6fc436dd10e4a01179631ea8ff
Devit Chea <devit.chea@wingbank.com.kh>
2025-02-22 16:14:59 +0700

    Update serializer

--- wdg_core_file_storage/serializers/file_storage_presigned_serializer.py
+++ wdg_core_file_storage/serializers/file_storage_presigned_serializer.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 
-
+class FileSerializer(serializers.Serializer):
+    original_file_name = serializers.CharField(max_length=255)
+    file_size = serializers.IntegerField()
+    content_type = serializers.CharField(max_length=50)
+    
+    
 class PreSingedUploadSerializer(serializers.Serializer):
-    file_id = serializers.CharField(max_length=64)
-    ref_type = serializers.CharField(max_length=64)
-    ref_id = serializers.IntegerField()
+    module = serializers.CharField(max_length=64, required=False)
+    classify = serializers.CharField(max_length=64, required=False)
+    ref_type = serializers.CharField(max_length=64, required=False)
+    ref_id = serializers.IntegerField(required=False)
+    files = FileSerializer(many=True, required=True)
 
 
 class DownloadPreSignedSerializer(serializers.Serializer):
