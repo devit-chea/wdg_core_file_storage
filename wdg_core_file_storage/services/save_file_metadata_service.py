9c202b19bee6297334cb8f8f4d72032c71868e3f
Devit Chea <devit.chea@wingbank.com.kh>
2025-02-23 13:06:12 +0700

    Add extra_fields for file metadata save and update.

--- wdg_core_file_storage/services/save_file_metadata_service.py
+++ wdg_core_file_storage/services/save_file_metadata_service.py
@@ -9,15 +9,20 @@ class SaveFileMetaService:
         cls,
         ref_type: str = None,
         ref_id: str = None,
-        user_id: str = None,
+        user_id: any = None,
         file_metadata_list: list = [],
+        extra_fields: dict = None,  # New argument for extra fields
     ):
         """
         Bulk creates FileStorageModel instances from a list of file metadata.
 
         Args:
+            ref_type (str),
+            ref_id (str),
+            user_id (any),
             file_metadata_list (list): List of dictionaries containing file metadata.
-                Example: [{"name": "file1.txt", "file_path": "/path/file1.txt", "size": 1024}, ...]
+                Example: [{"file_name": "file1.txt", "file_path": "/path/file1.txt", "file_size": 1024}, ...],
+            extra_fields={"custom_tag": "important"}
 
         Returns:
             list: file_metadata_list
@@ -25,20 +30,23 @@ class SaveFileMetaService:
         if not file_metadata_list:
             raise ValueError("File metadata list cannot be empty.")
 
+        extra_fields = extra_fields or {}  # Ensure it's a dictionary
+        
         # Prepare model instances
         file_instances = [
             FileStorageModel(
                 ref_id=ref_id,
                 ref_type=ref_type,
-                file_id=file.get("file_id"),
-                original_file_name=file.get("original_file_name"),
-                file_name=file.get("file_name"),
-                file_path=file.get("file_key"),
-                file_size=file.get("file_size"),
-                file_type=file.get("content_type"),
-                description=file.get("description"),
+                file_id=file.get("file_id", None),
+                original_file_name=file.get("original_file_name", None),
+                file_name=file.get("file_name", None),
+                file_path=file.get("file_path", None),
+                file_size=file.get("file_size", None),
+                file_type=file.get("file_type", None),
+                description=file.get("description", None),
                 create_date=datetime.now(),
                 create_uid=user_id,
+                **extra_fields,  # Unpacking extra fields
             )
             # Mapping through file meta data list
             for file in file_metadata_list
