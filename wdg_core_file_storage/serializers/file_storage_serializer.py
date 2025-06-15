fc70e95fc892f0d5dcfd4be10e0815fcc3aa9c30
devit chea <37470225+devit-chea@users.noreply.github.com>
2025-06-15 10:39:49 +0700

    Add FileStorageRelatedSerializer

--- wdg_core_file_storage/serializers/file_storage_serializer.py
+++ wdg_core_file_storage/serializers/file_storage_serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from wdg_core_file_storage.utils.ref_instance_map_util import build_ref_instance_map
 from wdg_core_file_storage.wdg_file_metadata.models import FileStorageModel
 
 
@@ -12,23 +13,24 @@ class FileStorageSerializer(serializers.ModelSerializer):
 class FileStorageValidateByRefSerializer(serializers.Serializer):
     ref_type = serializers.CharField()
     ref_id = serializers.IntegerField(required=False)
-    
+
+
 class FileStorageDeleteValidateSerializer(serializers.Serializer):
     id = serializers.CharField()
     file_path = serializers.CharField()
-    
-    
+
+
 class FileStoragePreviewValidateSerializer(serializers.Serializer):
     id = serializers.CharField()
     file_id = serializers.CharField()
     file_name = serializers.CharField()
-    
-    
+
+
 class FileStorageSaveMetaDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = FileStorageModel
         fields = "__all__"
-        
+
 
 class FileInfoSerializer(serializers.Serializer):
     original_file_name = serializers.CharField(required=True, allow_blank=False)
@@ -39,3 +41,27 @@ class FileInfoSerializer(serializers.Serializer):
 
 class FileStorageCreateValidateSerializer(serializers.Serializer):
     file_info = serializers.ListField(child=FileInfoSerializer(), allow_empty=False)
+
+
+class FileStorageRelatedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FileStorageModel
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if hasattr(self, "instance"):
+            self._ref_map = build_ref_instance_map(
+                self.instance if hasattr(self.instance, "__iter__") else [self.instance]
+            )
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        key = (instance.ref_type.lower(), instance.ref_id)
+        related_obj = self._ref_map.get(key)
+        if related_obj:
+            rep["related_name"] = getattr(
+                related_obj, "name", None
+            )  # or whatever field you want
+        return rep
